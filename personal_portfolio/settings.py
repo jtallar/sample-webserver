6bf026b6622507833a93c309e10b82d06f9a8334
Julian Tallar <julian.tallar@humand.co>
2022-06-24 00:42:38 -0300

    Trust ngrok and reenable debug

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -28,10 +28,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "!^_6%0so9$a@u-w22nc56xcp0^spoo4k^3q!j016o5hll+#c#o"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+CSRF_TRUSTED_ORIGINS = ["https://*.ngrok.io"]
 
 # Application definition
 
