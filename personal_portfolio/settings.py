f0e83dddc727ce9b75e05ff60fae29e6b05f9a51
Julian Tallar <julian.tallar@humand.co>
2022-06-24 00:33:14 -0300

    Allow all hosts + disable debug

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "!^_6%0so9$a@u-w22nc56xcp0^spoo4k^3q!j016o5hll+#c#o"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
